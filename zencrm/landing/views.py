a08173a6b27b3f8309ff835d0aa7abb996464ccd
FawazAshrafAli <fawazashrafali@gmail.com>
2024-05-03 09:46:50 +0530

    Connected url to Buy Crm button

--- zencrm/landing/views.py
+++ zencrm/landing/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, CreateView
 from crm_admin.models import Customer
 from django.urls import reverse_lazy
 from authentication.models import User
 from django.contrib.auth.hashers import make_password
+
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 
 class LandingPageView(TemplateView):
@@ -22,7 +25,7 @@ class ApplicationView(CreateView):
 
 
     def form_valid(self, form):
-        response = super().form_valid(form)
+        super().form_valid(form)
         customer = Customer.objects.latest("created")
         password = make_password(customer.organization_id)
         print(customer.organization_id)
@@ -34,18 +37,58 @@ class ApplicationView(CreateView):
             else:
                 first_name = customer.name
                 last_name = None
-        User.objects.create(
-            username = customer.email,
-            first_name = first_name,
-            last_name = last_name,
-            organization = customer.organization,
-            organization_id = customer.organization_id,
-            email = customer.email,
-            phone = customer.phone,
-            password = password,
+        try:
+            user = User.objects.create(
+                username = customer.email,
+                first_name = first_name,
+                last_name = last_name,
+                organization = customer.organization,
+                organization_id = customer.organization_id,
+                email = customer.email,
+                phone = customer.phone,
+                password = password,
 
-        )
-        return response
+            )
+        except Exception as e:
+            print(e)
+            return redirect(reverse_lazy('authentication:error500'))
+
+        try:
+            subject = "ZENCRM Account Created"
+            message = f"""ZENCRM account for your organization {user.organization} has been created.
+                        The current username for your account is your email id and the password is {user.organization_id}.
+                        You can change both the username and password later from your account settings."""
+            html_content = render_to_string('landing_email_templates/user_registration_response.html', {
+                'subject': subject,
+                'message': message,
+                "email_subject": subject,
+                "email_content": message,
+                "name": customer.name,
+                "username": user.username,
+                "organization": user.organization,
+                "password": user.organization_id
+                })            
+                    
+
+            email = EmailMessage(
+                subject=subject,
+                body=html_content,
+                from_email="w3digitalpmna@gmail.com",
+                to=["w3digitalpmna@gmail.com"]
+            )
+                        
+
+            # Set the content type of the email to 'text/html'
+            email.content_subtype = 'html'
+
+            # Send the email
+            email.send()
+
+            return redirect(self.get_success_url())
+
+        except Exception as e:
+            print(e)
+            return redirect(reverse_lazy('authentication:error500'))
 
     def form_invalid(self, form):
         for field, errors in form.errors.items():
